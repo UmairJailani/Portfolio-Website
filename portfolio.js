2f2eeb4d3f2cd02153bb994639920324a67d6c80
Muhammad Umair Jailani <umairjailani47@gmail.com>
2024-11-07 22:00:58 +0500

    new updates

--- portfolio.js
+++ portfolio.js
@@ -1,59 +1,18 @@
-const track = document.querySelector(".carousel-track");
-const items = Array.from(track.children);
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll("header nav a");
 
-const nextButton = document.querySelector(".carousel-button.right");
+window.onscroll = () => {
+  sections.forEach((section) => {
+    const top = window.scrollY;
+    const offset = section.offsetTop - 150;
+    const height = section.offsetHeight;
+    const id = section.getAttribute("id");
 
-const prevButton = document.querySelector(".carousel-button.left");
-
-document.getElementById("logo").addEventListener("click", function () {
-  document.querySelector("#home").scrollIntoView({
-    behavior: "smooth",
-    block: "center",
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((link) => link.classList.remove("active"));
+      document
+        .querySelector(`header nav a[href*="${id}"]`)
+        .classList.add("active");
+    }
   });
-});
-
-document.querySelectorAll("nav a").forEach((anchor) => {
-  anchor.addEventListener("click", function (event) {
-    event.preventDefault();
-
-    const targetSection = document.querySelector(this.getAttribute("href"));
-
-    targetSection.scrollIntoView({
-      behavior: "smooth",
-      block: "center",
-      inline: "center",
-    });
-  });
-});
-
-const itemWidth = items[0].getBoundingClientRect().width;
-let currentIndex = 0;
-
-// Arrange items in a row (if not full width)
-items.forEach((item, index) => {
-  item.style.left = itemWidth * index + "px";
-});
-
-function updateCarousel(position) {
-  track.style.transform = "translateX(-" + position + "px)";
-}
-
-// Move to next item
-nextButton.addEventListener("click", () => {
-  if (currentIndex < items.length - 1) {
-    currentIndex++;
-  } else {
-    currentIndex = 0; // Loop back to the first item
-  }
-  updateCarousel(currentIndex * itemWidth);
-});
-
-// Move to previous item
-prevButton.addEventListener("click", () => {
-  if (currentIndex > 0) {
-    currentIndex--;
-  } else {
-    currentIndex = items.length - 1; // Loop back to the last item
-  }
-  updateCarousel(currentIndex * itemWidth);
-});
+};
